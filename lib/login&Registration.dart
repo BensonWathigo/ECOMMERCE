3db9d39f6c668e57bdba7f44aa65808d17c5e024
BensonWathigo <bensonwathigo21@gmail.com>
2024-05-28 15:21:08 +0300

    first commit

--- lib/login&Registration.dart
+++ lib/login&Registration.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -5,6 +6,35 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return scaffold();
+    return Scaffold(
+      backgroundColor: Color.grey[300],
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(height: 50,),
+
+            // logo
+            Icon(Icons.lock,
+            size: 100,
+            )
+            const SizedBox(height: 50 ),
+        
+            // welcome. You've been missed.
+            
+        
+            // forgot password 
+        
+            // signin button
+        
+            //or continue with
+        
+            // gooogle or apple sign in button 
+        
+            // not a member ? register
+        
+          ],
+        ),
+      ),
+    )
   }
 }
