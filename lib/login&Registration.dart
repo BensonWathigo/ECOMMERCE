cb275d70306eef8f035bb9c8f32862feb13205c1
BensonWathigo <bensonwathigo21@gmail.com>
2024-05-28 20:31:49 +0300

    first commit

--- lib/login&Registration.dart
+++ lib/login&Registration.dart
@@ -1,55 +1,121 @@
-
 import 'package:flutter/material.dart';
+import 'package:modernlogintute/components/my_button.dart';
+import 'package:modernlogintute/components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
+  //Text editing controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  //sign user in method
+  void signUserIn() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.grey[300],
+      backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const SizedBox(height: 50,),
-
+            const SizedBox(
+              height: 50,
+            ),
             // logo
-            Icon(Icons.lock,
-            size: 100,
-            )
-            const SizedBox(height: 50 ),
-        
+            const Icon(
+              Icons.lock,
+              size: 100,
+            ),
+            const SizedBox(height: 50),
+
             // welcome. You've been missed.
             Text(
               "Welcome, you've been missed.",
               style: TextStyle(
-                color: Color.grey[700],
+                color: Colors.grey[700],
                 fontSize: 16,
               ),
-            )
-            const SizedBox(height: 50),
+            ),
 
-            // Username verified
-            my_textField(),
-            const SizedBox(height: 50),
+            const SizedBox(height: 25),
 
+            // forgot password
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                children: [
+                  Text(
+                    'Forgot password',
+                    style: TextStyle(color: Colors.amber.shade200),
+                  ),
+                ],
+              ),
+            ),
 
-            //password textified 
-            my_textField(), 
-        
-            // forgot password 
-        
-            // signin button
-        
+            const SizedBox(height: 25),
+
             //or continue with
-        
-            // gooogle or apple sign in button 
-        
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Text(
+                'Or continue with',
+                style: TextStyle(color: Colors.green[400]),
+              ),
+            ),
+
+            Expanded(
+              child: Divider(
+                thickness: 0.5,
+                color: Colors.grey[400],
+              ),
+            ),
+
+            // gooogle or apple sign in button
+            Row(
+              children: [
+                //Google sign in buttons
+                Image.asset(
+                  'lib\Images\Google.jpg',
+                  height: 72,
+                ),
+              ],
+            ),
+
             // not a member ? register
-        
+            Row(
+              children: [
+                Text(
+                  'Not a member?',
+                  style: TextStyle(color: Colors.grey[700]),
+                ),
+                const SizedBox(width: 4),
+                Text(
+                  'Register now!',
+                  style: TextStyle(
+                      color: Colors.yellow[400], fontWeight: FontWeight.bold),
+                ),
+              ],
+            )
           ],
         ),
       ),
-    )
+    );
   }
 }
